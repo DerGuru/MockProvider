3acc58ea8cadcd31a290c77515012181f71f5b92
Andreas Jakof <JakofHe@basfad.basf.net>
2020-09-24 16:48:02 +0200

    init

--- MockProvider/MockProvider.cs
+++ MockProvider/MockProvider.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 
 public class MockProvider : IList<ServiceDescriptor>, IServiceProvider, IServiceCollection
 {
